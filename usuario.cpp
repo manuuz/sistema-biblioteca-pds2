c19429d358b24d764e498fa20289820061c29769
isaquino <160413638+isaquino@users.noreply.github.com>
2024-06-06 08:09:07 -0300

    Create usuario.cpp

---
+++ usuario.cpp
@@ -0,0 +1,45 @@
+#include "usuario.h"
+#include <algorithm>
+
+Usuario::Usuario(int id, std::string nome) : id(id), nome(nome), multas(0.0) {}
+
+int Usuario::getId() {
+    return id;
+}
+
+std::string Usuario::getNome() {
+    return nome;
+}
+
+double Usuario::getMultas() {
+    return multas;
+}
+
+void Usuario::adicionarMulta(double valor) {
+    multas += valor;
+}
+
+void Usuario::removerMulta(double valor) {
+    if (valor <= multas) {
+        multas -= valor;
+    }
+}
+
+void Usuario::emprestarLivro(Livro* livro) {
+    if (livro->isDisponivel()) {
+        livrosEmprestados.push_back(livro);
+        livro->setDisponivel(false);
+    }
+}
+
+void Usuario::devolverLivro(Livro* livro) {
+    auto it = std::find(livrosEmprestados.begin(), livrosEmprestados.end(), livro);
+    if (it != livrosEmprestados.end()) {
+        livrosEmprestados.erase(it);
+        livro->setDisponivel(true);
+    }
+}
+
+std::vector<Livro*> Usuario::getLivrosEmprestados() {
+    return livrosEmprestados;
+}
