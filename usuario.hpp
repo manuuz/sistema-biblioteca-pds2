91e7b1a3497f8baf9924f6d54d4093de9c0b2061
isaquino <160413638+isaquino@users.noreply.github.com>
2024-06-06 08:08:39 -0300

    Create usuario.hpp

---
+++ usuario.hpp
@@ -0,0 +1,27 @@
+#ifndef USUARIO_HPP
+#define USUARIO_HPP
+
+#include <string>
+#include <vector>
+#include "livro.hpp"
+
+class Usuario {
+private:
+    int id;
+    std::string nome;
+    std::vector<Livro*> livrosEmprestados;
+    double multas;
+
+public:
+    Usuario(int id, std::string nome);
+    int getId();
+    std::string getNome();
+    double getMultas();
+    void adicionarMulta(double valor);
+    void removerMulta(double valor);
+    void emprestarLivro(Livro* livro);
+    void devolverLivro(Livro* livro);
+    std::vector<Livro*> getLivrosEmprestados();
+};
+
+#endif // USUARIO_HPP
