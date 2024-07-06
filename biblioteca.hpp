719edc2d0a6031b387d4fa5b155b5b995e082ae3
maluoli12 <141967003+maluoli12@users.noreply.github.com>
2024-07-06 17:24:51 -0300

    Create biblioteca.hpp

---
+++ biblioteca.hpp
@@ -0,0 +1,24 @@
+#ifndef BIBLIOTECA_HPP
+#define BIBLIOTECA_HPP
+
+#include <vector>
+#include "livro.hpp"
+#include "usuario.hpp"
+#include "funcionario.hpp"
+
+class Biblioteca {
+private:
+    std::vector<Livro*> livros;
+    std::vector<Usuario*> usuarios;
+    std::vector<Funcionario*> funcionarios;
+
+public:
+    void adicionarLivro(Livro* livro);
+    void adicionarUsuario(Usuario* usuario);
+    void adicionarFuncionario(Funcionario* funcionario);
+    void emprestarLivro(int livroID, int usuarioID);
+    void devolverLivro(int livroID, int usuarioID);
+    void exibirEstoque() const;
+};
+
+#endif // BIBLIOTECA_HPP
